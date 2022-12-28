4a8a18ca6a5e5be1f8be8c20babe300a9f2e5e69
isaac-svg <isaacsakyi009@gmail.com>
2022-12-28 19:51:07 +0000

    working on storage and backgrounds

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const app = () => {
+  return <div>app askjksjdfsldsljflsdflkdasjdsaljasldsalk</div>;
+};
+
+export default app;
--- src/assets/URL.jsx
+++
@@ -1,1 +0,0 @@
-export const URL = "https://restcountries.com/v3.1";
